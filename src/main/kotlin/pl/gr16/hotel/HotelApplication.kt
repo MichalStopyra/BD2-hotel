484d0b7c5c7fd883c1b1cb0301832fab8884a0a3
Michal <michalstopyra1@gmail.com>
2020-12-28 15:35:32 +0100

    add project template

---
+++ src/main/kotlin/pl/gr16/hotel/HotelApplication.kt
@@ -0,0 +1,11 @@
+package pl.gr16.hotel
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HotelApplication
+
+fun main(args: Array<String>) {
+	runApplication<HotelApplication>(*args)
+}
